7d64c4cf642b3632847c8e42ec42bd375381e1dd
regenzy2020 <alfredovalerga@hotmail.com>
2021-06-24 20:49:37 -0300

    javaC2
    Clase 2 actividad

---
+++ java-clase2.js
@@ -0,0 +1,31 @@
+// const numeroSuertudo = parseInt(prompt("Ingrese Número"));
+// if (numeroSuertudo > 100) {
+//     alert("Te fuiste de mambo, volve a intentar");
+// }
+// else if (numeroSuertudo <= 10){
+//     alert("ohhh increible");
+// }
+// else if (numeroSuertudo <= 50){
+//     alert("Buena suerte");
+// }
+// else if ((numeroSuertudo >= 80) && (numeroSuertudo <= 90)){
+//     alert("sigue asi")
+// }
+
+// const saludo = prompt("ingresar saludo")
+// if ((saludo != "") && ((saludo = "hola genio") || (saludo == "HOLA GENIO"))){
+//     console.log("hola Brother");
+// }
+// else {
+//     console.log("error: vuelve a intentar");
+// }
+
+// const nombreIg = prompt("Ig Nombre");
+// const apellidoIg = prompt("Ig Apellido");
+
+// if((nombreIg != "") && (apellidoIg != "")){
+//     console.log("Nombre: "+nombreIg +"\n Apellido: "+apellidoIg);
+// }
+// else{
+//     console.log("Error: Ingrese NOMBRE y APELLIDO");
+// }
