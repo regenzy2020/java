653618549db85fb8e9f2e04178503e17cc452981
regenzy2020 <alfredovalerga@hotmail.com>
2021-06-29 20:10:23 -0300

    java-clase-3
    ejercicios

---
+++ java-clase3/java-clase3.js
@@ -0,0 +1,51 @@
+// CICLO FOR ++
+
+for (let i = 1; i <=5; i++) {
+    console.log(`interacion ${i}`);
+}
+console.log('Fin del Ciclo');
+
+// let dias =["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+// for (i = 0 ; i <= dias.length - 1; i++){
+//     document.write (dias[i] + " <br> ");
+// }
+
+// CICLO FOR --
+
+// for (let i = 5; i >=1; i--) {
+//     console.log(`interacion ${i}`);
+// }
+// console.log('Fin del Ciclo');
+
+// CICLO ANIDADO
+
+// for (let i = 1; i <=5; i++) {
+//     console.log(`Empieza interacion ${i}`);
+//     for(let j = 0; j<4; j++){
+//         console.log(j);
+//     }
+// }
+// console.log('Fin del Ciclo');
+
+// WHILE
+
+// let x = 1;
+// while (x <= 4) {
+//     console.log(`Interacion ${x}`);
+//     x++;
+// }
+// console.log(`Fin de While`)
+
+// DO WHILE
+
+// let y = 1;
+// do {
+//     console.log(`Interacion ${y}`);
+//     y++;
+// } while (y <= 8);
+
+// let y = 9;
+// do {
+//     console.log(`Interacion ${y}`);
+//     y++;
+// } while (y <= 8);
