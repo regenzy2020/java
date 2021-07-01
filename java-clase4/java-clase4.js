d2f12faaec6c8e2b2c6cd037557460bed5d93be5
regenzy2020 <alfredovalerga@hotmail.com>
2021-07-01 20:26:23 -0300

    java-clase4
    actividades

---
+++ java-clase4/java-clase4.js
@@ -0,0 +1,28 @@
+
+// function saludo(nombre){
+//     document.write("Hola" + " " + nombre + " " + "que tenga un buen día");
+// }
+// saludo("Profesor");
+
+// ---------------------------------------------------------------------
+
+// var suma = function suma(numero1, numero2) {
+//     var numero1 = numero1;
+//     var numero2 = numero2;
+
+//     return numero1 + numero2;
+// }
+// document.write(suma(10,40));
+// document.write ("<br>");
+// document.write(suma(30,70));
+
+// ---------------------------------------------------------------------
+
+// const suma  = (a,b) => a + b;
+// const resta = (a,b) => a - b;
+// const iva   = x => x * 0.21;
+// let precioProducto  = 200; 
+// let precioDescuento = 80;
+
+// let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento); 
+// console.log(nuevoPrecio);
